b197503d11c2ee2fe3fc9a4168563a59f7e61da9
kamol-nazarov <kamol.nazarov15@myhunter.cuny.edu>
2020-01-06 16:36:09 -0500

    fixed 100%

--- mini_max_sum.php
+++ mini_max_sum.php
@@ -4,40 +4,75 @@ function miniMaxSum($arr) {
 
   $size = sizeof( $arr );
 
-  for ( $i=0; $i<$size; $i++ ) {
+  $smallest = array( 0, $arr[0] );
+  $biggest = array( 0, $arr[0] );
+  $result = array ( 0, 0 );
 
-    $answers = array();
+  for ( $i=0; $i < $size; $i++ ) {
 
-    for ( $j=0; $j<$size; $j++ ) {
+    if ( $smallest[1] > $arr[$i] ) {
 
-      if ( $j === $i ) 
-        continue;
+      $smallest[0] = $i;
+      $smallest[1] = $arr[$i];
 
-      $answers[] = $arr[$j];
-     
-    }
-
-    $size_answers = sizeof( $answers );
-    $final = 0;
-
-    for ( $k=0; $k<$size_answers; $k++ ) {
-
-      $final += $answers[$k];
+      // print_r( "NEW SMALLEST : {$smallest[0]} {$smallest[1]} ");
+      // echo "\n";
 
     }
-
-    echo "\n";
-    print_r( $final );
-    echo "\n";
     
+    if ( $biggest[1] < $arr[$i] ) {
+
+      $biggest[0] = $i;
+      $biggest[1] = $arr[$i];
+
+      // print_r( "NEW BIGGEST : {$biggest[0]} {$biggest[1]}");
+      // echo "\n";
+
+    }
+
+  } 
+  
+  
+  for ( $i=0; $i < $size; $i++ ) {
+
+    if ( $i === $biggest[0] ) {
+
+      // print_r( "FOUND BIGGEST : {$arr[$i]} " );
+      // echo "\n";
+
+      continue;
+
+    }
+
+    $result[0] += $arr[$i];
+
   }
 
-  return;
+  for ( $i=0; $i < $size; $i++ ) {
 
+    if ( $i === $smallest[0] ) {  
+
+      // print_r( "FOUND SMALLEST : {$arr[$i]} " );
+      // echo "\n";
+
+      continue;    
+
+    }
+
+    $result[1] += $arr[$i];
+
+  }
+
+
+  print_r( "{$result[0]} {$result[1]}");
+  echo "\n";
+
+  return;
+  
 }
 
 
-$arr = array( 1, 2, 5, 7, 9 );
+$arr = array( 1, 2, 3, 4, 5 );
 
 miniMaxSum($arr);
 
