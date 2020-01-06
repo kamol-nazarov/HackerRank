e57c5e7e925fd2cede30eebd47b88cdf8c4736d1
kamol-nazarov <kamol.nazarov15@myhunter.cuny.edu>
2020-01-06 15:37:38 -0500

    mini-max sum

---
+++ mini_max_sum.php
@@ -0,0 +1,45 @@
+<?php
+
+function miniMaxSum($arr) {
+
+  $size = sizeof( $arr );
+
+  for ( $i=0; $i<$$size; $i++ ) {
+
+    $answers = array();
+
+    for ( $j=0; $j<$size; $j++ ) {
+
+      if ( $j === $i ) continue;
+
+      $answers[] += $arr[$j];
+     
+    }
+
+    $size_answers = sizeof( $answers );
+    $final = 0;
+
+    for ( $k=0; $k<$size_answers; $k++ ) {
+
+      $final += $answers[$k]
+
+    }
+
+    print_r( $final );
+    echo "\n";
+    
+  }
+
+  return;
+
+}
+
+
+$arr = array( 1, 2, 5, 7, 9 );
+
+miniMaxSum($arr);
+
+
+
+
+?>
