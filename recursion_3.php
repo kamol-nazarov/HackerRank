a06663e039d2c037a4edbe8f5996f951f038e856
kamol-nazarov <kamol.nazarov15@myhunter.cuny.edu>
2020-01-17 13:29:54 -0500

    Recursion 3

---
+++ recursion_3.php
@@ -0,0 +1,26 @@
+<?php
+
+// Complete the factorial function below.
+function factorial($n) {
+
+    if ( $n === 1 )
+        return $n;
+
+    return $n * factorial( $n - 1 );
+
+}
+
+$fptr = fopen(getenv("OUTPUT_PATH"), "w");
+
+$stdin = fopen("php://stdin", "r");
+
+fscanf($stdin, "%d\n", $n);
+
+$result = factorial($n);
+
+fwrite($fptr, $result . "\n");
+
+fclose($stdin);
+fclose($fptr);
+
+?>
